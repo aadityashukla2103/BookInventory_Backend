3bb278256cadabc41516099ba9df3e629808f03b
jahnvi1177 <jahnvi1026@gmail.com>
2026-04-24 19:53:23 +0530

    Initial commit

--- SpringBootBookInventory/src/main/java/com/cg/entities/Book.java
+++ SpringBootBookInventory/src/main/java/com/cg/entities/Book.java
@@ -1,5 +1,80 @@
 package com.cg.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @Column(name = "ISBN", length = 13)
+    private String isbn;
+
+    @Column(name = "Title", length = 70, nullable = false)
+    private String title;
+
+    @Column(name = "Description", length = 100)
+    private String description;
+
+    @Column(name = "Edition", length = 30)
+    private String edition;
+
+    
+    @ManyToOne
+    @JoinColumn(name = "Category")   
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "PublisherID", nullable = false)
+    private Publisher publisher;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+
+    
 }
--- SpringBootBookInventory/src/main/java/com/cg/entities/Publisher.java
+++ SpringBootBookInventory/src/main/java/com/cg/entities/Publisher.java
@@ -1,5 +1,80 @@
 package com.cg.entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "publisher")
 public class Publisher {
 
+    @Id
+    @Column(name = "PublisherID")
+    private Integer publisherId;
+
+    @Column(name = "Name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "City", length = 30)
+    private String city;
+
+    
+    @ManyToOne
+    @JoinColumn(name = "StateCode")
+    private State state;
+
+   
+    @OneToMany(mappedBy = "publisher")
+    private List<Book> books;
+
+
+	public Integer getPublisherId() {
+		return publisherId;
+	}
+
+
+	public void setPublisherId(Integer publisherId) {
+		this.publisherId = publisherId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public State getState() {
+		return state;
+	}
+
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+    
 }
