82c083dd7e90c5a8f8689227a9def0eca53feafc
Bha108jain <bhavyajainn1008@gmail.com>
2026-04-26 13:35:29 +0530

    error fix

--- SpringBootBookInventory/src/main/java/com/cg/entity/PermRole.java
+++ SpringBootBookInventory/src/main/java/com/cg/entity/PermRole.java
@@ -1,3 +1,9 @@
+package com.cg.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "permrole")
 public class PermRole {
