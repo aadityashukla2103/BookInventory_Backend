ddd589068ebee4cacc71bb24c3fcc57f96ac160e
Bha108jain <bhavyajainn1008@gmail.com>
2026-04-27 12:45:24 +0530

    added dto bookreview and reviewer

---
+++ SpringBootBookInventory/src/main/java/com/cg/dto/BookReviewDto.java
@@ -0,0 +1,40 @@
+package com.cg.dto;
+
+public class BookReviewDto {
+	private String isbn;
+	private Integer reviewerId;
+	private Integer rating;
+	private String comments;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Integer getReviewerId() {
+		return reviewerId;
+	}
+
+	public void setReviewerId(Integer reviewerId) {
+		this.reviewerId = reviewerId;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+}
