cfb6969d0939153313aa981033695d5843901199
jahnvi1177 <jahnvi1026@gmail.com>
2026-04-28 11:48:44 +0530

    dto commit

--- SpringBootBookInventory/src/main/java/com/cg/dto/PublisherDto.java
+++ SpringBootBookInventory/src/main/java/com/cg/dto/PublisherDto.java
@@ -1,5 +1,24 @@
 package com.cg.dto;
 
 public class PublisherDto {
+    private Integer publisherId;
+    private String name;
+    private String city;
+    private String stateCode;
 
+    public Integer getPublisherId() { return publisherId; }
+    
+    public void setPublisherId(Integer publisherId) { this.publisherId = publisherId; }
+    
+    public String getName() { return name; }
+    
+    public void setName(String name) { this.name = name; }
+    
+    public String getCity() { return city; }
+    
+    public void setCity(String city) { this.city = city; }
+    
+    public String getStateCode() { return stateCode; }
+    
+    public void setStateCode(String stateCode) { this.stateCode = stateCode; }
 }
