6a15fb7cef5db984da313bd1fb37432a01ff2b8a
jahnvi1177 <jahnvi1026@gmail.com>
2026-04-28 11:51:05 +0530

    dto

--- SpringBootBookInventory/src/main/java/com/cg/dto/BookDto.java
+++ SpringBootBookInventory/src/main/java/com/cg/dto/BookDto.java
@@ -1,5 +1,34 @@
 package com.cg.dto;
 
 public class BookDto {
+    private String isbn;
+    private String title;
+    private String description;
+    private String edition;
+    private Integer categoryId;
+    private Integer publisherId;
 
+    public String getIsbn() { return isbn; }
+    
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    
+    public String getTitle() { return title; }
+    
+    public void setTitle(String title) { this.title = title; }
+    
+    public String getDescription() { return description; }
+    
+    public void setDescription(String description) { this.description = description; }
+    
+    public String getEdition() { return edition; }
+    
+    public void setEdition(String edition) { this.edition = edition; }
+    
+    public Integer getCategoryId() { return categoryId; }
+    
+    public void setCategoryId(Integer categoryId) { this.categoryId = categoryId; }
+    
+    public Integer getPublisherId() { return publisherId; }
+    
+    public void setPublisherId(Integer publisherId) { this.publisherId = publisherId; }
 }
