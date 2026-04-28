4f46cb691afc94cf56e4228c44e388bd78bef6ea
Bhavya Jain <138205097+Bha108jain@users.noreply.github.com>
2026-04-28 11:29:28 +0530

    Revert "Added Dto"

--- SpringBootBookInventory/java/com/cg/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cg.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    Customer findByEmailAndPassword(String email, String password);
-}
--- SpringBootBookInventory/java/com/cg/repository/RouteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cg.entity.Route;
-
-public interface RouteRepository extends JpaRepository<Route, Integer> {
-}
