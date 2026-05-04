e260dbebe04e9d3f1823fc2b762e68d16efb54ed
Bha108jain <bhavyajainn1008@gmail.com>
2026-05-05 02:19:52 +0530

    fixing the styling

--- book_inventory_frontend/src/app/book-card/book-card.component.spec.ts
+++ book_inventory_frontend/src/app/book-card/book-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { appTestProviders } from '../testing/app-test-providers';
 import { BookCardComponent } from './book-card.component';
 
 describe('BookCardComponent', () => {
@@ -8,12 +9,23 @@ describe('BookCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BookCardComponent]
+      imports: [BookCardComponent],
+      providers: appTestProviders
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(BookCardComponent);
     component = fixture.componentInstance;
+    component.book = {
+      isbn: '9780000000000',
+      title: 'Sample Book',
+      authorNames: [],
+      categoryName: 'Reference',
+      publisherName: 'Demo Press',
+      description: 'Test description',
+      availableCopies: 1,
+      priceLabel: '$10.00'
+    };
     fixture.detectChanges();
   });
 
